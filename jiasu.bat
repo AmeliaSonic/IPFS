5e287936b4edfc2b408a7dc7ffbbabb05b8d37b1
AmeliaSonic <47511744+AmeliaSonic@users.noreply.github.com>
2019-02-26 16:28:09 +0800

    Add files via upload
    添加加速脚本

---
+++ jiasu.bat
@@ -0,0 +1,7 @@
+@set path = %path%;%~dp0
+goto start
+:start
+ipfs ping QmZW56GovefKJib8anp2qT293yNK2AFxMsdViXDAgthhLG
+ipfs ping QmYfakhU79QoJBEkhCb4ZETo5J3VFtMSwAtrgrKsDBEeoT
+ipfs ping QmREzmYBLoLayaoh2Rb7AHDf1TbmrRFEX5P2bJmHPM8E16
+goto start
